cab5253d18f1e5cf92575321d511268ccec7ba6d
suntong <1940659415@qq.com>
2019-08-29 18:50:29 +0800

    跨域过滤器

---
+++ src/main/java/com/qfedu/examsys/filter/CrossFilter.java
@@ -0,0 +1,31 @@
+package com.qfedu.examsys.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "CrossFilter",urlPatterns = "/*")
+public class CrossFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+
+        // token,name是程序中自定义的请求头，在 Access-Control-Allow-Headers进行设置
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,accept,content-type,token,name");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE,PUT");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
