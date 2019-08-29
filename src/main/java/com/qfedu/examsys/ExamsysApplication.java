bfd669f0b3fd71236c95ddc338d488ae41e8e634
suntong <1940659415@qq.com>
2019-08-29 18:44:38 +0800

    添加servlet注解扫描

--- src/main/java/com/qfedu/examsys/ExamsysApplication.java
+++ src/main/java/com/qfedu/examsys/ExamsysApplication.java
@@ -3,9 +3,11 @@ package com.qfedu.examsys;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.qfedu.examsys.dao")
+@ServletComponentScan // 扫描 servlet相关的注解
 public class ExamsysApplication {
 
     public static void main(String[] args) {
