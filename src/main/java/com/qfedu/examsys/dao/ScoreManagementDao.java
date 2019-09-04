4d1bb6b8aece430ec87a3cf56e127a09d2fc7b19
dongge <15139199541@139.com>
2019-09-04 09:36:44 +0800

    查询成绩1

--- src/main/java/com/qfedu/examsys/dao/ScoreManagementDao.java
+++ src/main/java/com/qfedu/examsys/dao/ScoreManagementDao.java
@@ -1,9 +1,17 @@
 package com.qfedu.examsys.dao;
 
+import com.qfedu.examsys.entity.Subject;
 import com.qfedu.examsys.vo.VScoreManage;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface ScoreManagementDao {
-    List<VScoreManage> findAllScoreManage(Integer sid);
+    List<VScoreManage> findAllScoreManage(HashMap<Object,Object> map);
+
+    VScoreManage findByIdScoreManage(Integer sid);
+
+    List<Subject> findAllSubject();
+
+    int updateScoreManage(VScoreManage vScoreManage);
 }
--- src/main/java/com/qfedu/examsys/service/ScoreManagementService.java
+++ src/main/java/com/qfedu/examsys/service/ScoreManagementService.java
@@ -1,7 +1,15 @@
 package com.qfedu.examsys.service;
 
+import com.qfedu.examsys.vo.VScoreManage;
+
 import java.util.Map;
 
 public interface ScoreManagementService {
-    Map<String, Object> findAllScoreManage(Integer page, Integer limit, Integer sid);
+    Map<String, Object> findAllScoreManage(Integer page, Integer limit, String studentName, String subjectName, Integer eid);
+
+    Map<String, Object> findByIdScoreManage(Integer sid);
+
+    Map<String, Object> findAllSubject();
+
+    Map<String, Object> updateScoreManage(VScoreManage vScoreManage);
 }
